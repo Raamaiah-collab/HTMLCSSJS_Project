936555dacdb1c733a9fd645d567ea80c7a2d7dc5
Raamaiah-collab <Raamaiahjangala@gmail.com>
2025-12-14 22:51:48 +0530

    new changes check

---
+++ script.js
@@ -0,0 +1,16 @@
+alertFunction = () => {
+    alert("Button clicked! you Dont have Permission to Delete.");
+} ;
+
+function myFunction() {
+    document.getElementById("changeH1").innerHTML = "You have clicked the Button!";
+}
+
+
+function saveFunction() {
+    document.getElementById("changeH1").innerHTML = "You have clicked the Save Button!";
+} ;
+
+function ChangeH1Name() {
+    console.log("This is another function.");
+}
